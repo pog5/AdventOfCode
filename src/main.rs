ca09f48a50132531e02f2f2cadc0dd44e83b3803
Nightly <5535krisi+github.com@gmail.com>
2024-12-03 20:27:09 +0200

    day 2: init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::io::Write;
 use crate::day1::day1;
+use crate::day2::day2;
 
 mod day1;
+mod day2;
 
 fn main() {
-    day1();
+    print!("Day: ");
+    std::io::stdout().flush().expect("cant flush stdout");
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let day = input.trim().parse::<u32>().unwrap();
+
+    match day {
+        1 => day1(),
+        2 => day2(),
+        _ => println!("Day not implemented"),
+    }
 }
 
