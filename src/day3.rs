4728d5e6a08cf8b2bbfcd337c1c836edafd72a0c
Nightly <5535krisi+github.com@gmail.com>
2024-12-03 23:33:53 +0200

    day 3: part one done

--- src/day3.rs
+++ src/day3.rs
@@ -1,5 +1,16 @@
+use reformation::Regex;
+
 fn part1(input: &str) -> String {
-    "TODO".to_string()
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut numbers = Vec::new();
+    for (_, [first, second]) in re.captures_iter(input).map(|c| c.extract()) {
+        let firstnum: i128 = first.parse::<i128>().unwrap();
+        let secondnum: i128 = second.parse::<i128>().unwrap();
+        numbers.push(firstnum * secondnum);
+    }
+    
+    let sum: i128 = numbers.iter().sum();
+    sum.to_string()
 }
 
 fn part2(input: &str) -> String {
