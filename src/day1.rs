629035942628b30a4e0f13cc1a2ad5b7f5ab360b
Nightly <5535krisi+github.com@gmail.com>
2024-12-03 20:18:00 +0200

    day 1: done

--- src/day1.rs
+++ src/day1.rs
@@ -1,48 +1,37 @@
-use std::collections::HashMap;
 use reformation::{Reformation, Regex};
-use sorted_vec::SortedVec;
 
-struct NumberPair {
-    first: Vec<i128>,
-    second: Vec<i128>,
-}
-
-impl NumberPair {
-    fn joinWithAnother(&self, other: &NumberPair) -> NumberPair {
-        NumberPair {
-            first: self.first.iter().chain(other.first.iter()).cloned().collect(),
-            second: self.second.iter().chain(other.second.iter()).cloned().collect(),
-        }
+fn dist_between_nums(a: i128, b: i128) -> i128 {
+    if a > b {
+        a - b
+    } else {
+        b - a
     }
 }
-
 pub fn day1() {
     let input = include_str!("../input/day1.txt");
-    let re = Regex::new(r"(\d)\s(\d)").unwrap();
-    let mut numbers = input
-        .lines()
-        .map(|line| {
-            println!("{}", line);
-            let caputredLine = re.captures(line).unwrap();
-            NumberPair {
-                first: vec![caputredLine.get(1).unwrap().as_str().parse::<i128>().unwrap()],
-                second: vec![caputredLine.get(2).unwrap().as_str().parse::<i128>().unwrap()],
-            }
-        })
-        .collect::<Vec<NumberPair>>();
-    let mut allNumbers = numbers.pop().unwrap();
-    let mut finalPair = numbers.iter().fold(allNumbers, |acc, number| acc.joinWithAnother(number));
+    let re = Regex::new(r"(\d+)\s+(\d+)").unwrap();
+    let mut first_list = Vec::new();
+    let mut second_list = Vec::new();
 
-    finalPair.first.sort_by(|a, b| b.cmp(a));
-    finalPair.second.sort_by(|a, b| b.cmp(a));
-
-    let mut sums = Vec::new();
-    for first in finalPair.first.iter() {
-        for second in finalPair.second.iter() {
-            sums.push(first - second);
-        }
+    for line in input.lines() {
+        let captured = re.captures(line).expect("Can't capture from line");
+        first_list.push(captured.get(1).unwrap().as_str().parse::<i128>().unwrap());
+        second_list.push(captured.get(2).unwrap().as_str().parse::<i128>().unwrap());
     }
 
-    let sum: i128 = sums.iter().sum();
+    first_list.sort();
+    second_list.sort();
+
+    println!("{:?}", first_list);
+    println!("{:?}", second_list);
+
+    let mut nums = first_list.iter().zip(second_list.iter())
+        .map(|(a, b)| dist_between_nums(*a, *b))
+        .collect::<Vec<i128>>();
+
+    nums.sort();
+    println!("{:?}", nums);
+
+    let sum: i128 = nums.iter().sum();
     println!("{}", sum);
 }
