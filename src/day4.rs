7c6fb47a6f97e54425faa6825704fd0eee476257
Nightly <5535krisi+github.com@gmail.com>
2024-12-04 12:49:48 +0200

    day 4: start

---
+++ src/day4.rs
@@ -0,0 +1,17 @@
+fn part1(input: &str) -> String {
+    "TODO".to_string()
+}
+fn part2(input: &str) -> String {
+    "TODO".to_string()
+}
+
+pub fn day4() {
+    let input = include_str!("../input/day4.txt");
+    let response = part1(input);
+    println!();
+    let response2 = part2(input);
+
+    println!();
+    println!("Part One: {}", response);
+    println!("Part Two: {}", response2);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use std::io::Write;
 use crate::day1::day1;
 use crate::day2::day2;
 use crate::day3::day3;
+use crate::day4::day4;
 
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     print!("Day: ");
     std::io::stdout().flush().expect("cant flush stdout");
-
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let day = input.trim().parse::<u32>().unwrap();
@@ -19,6 +20,7 @@ fn main() {
         1 => day1(),
         2 => day2(),
         3 => day3(),
+        4 => day4(),
         _ => println!("Day not implemented"),
     }
 }
